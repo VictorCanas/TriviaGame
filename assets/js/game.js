a48bb31401b5902c03a61ff71a3340dccea38deb
Victor <victorcanasint@gmail.com>
2017-12-02 19:20:22 -0500

    modified javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+    $('#statext').hide();
+    $('#image').hide();
+    $('#distext').hide();
+    $('#button').hide();
+});
+
+
 // This array holds the words we are going to choose from.
 var words = ['eleven', 'demogorgon', 'barbara', 'will', 'lucas'];
 
@@ -18,6 +26,30 @@ document.getElementById("distext").innerHTML = word;
 //This gets the image but it also gets the image of what it's being guessed
 //This is done creatively by using the variable word and having the image file match the array of word 
 document.getElementById('image').src = "assets/images/" + word + ".png";
+
+function startTimer(){
+  var counter = 10;
+  setInterval(function() {
+    counter--;
+    if (counter >= 0) {
+      span = document.getElementById("count");
+      span.innerHTML = counter;
+    }
+    if (counter === 0) {
+        alert('sorry, out of time');
+        clearInterval(counter);
+    }
+  }, 1000); 
+
+}
+$("#startClock").click(function(){
+    startTimer();
+    $('#startClock').hide();
+    $('#statext').show();
+    $('#image').show();
+    $('#distext').show();
+    $('#button').show();
+ });
 
 
 function displayStatus(status)
