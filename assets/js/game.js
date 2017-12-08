2137e8264c3b586f7721c67569de88dd81f6aef8
Victor Cañas <victorcanasint@gmail.com>
2017-12-08 16:33:39 -0500

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,17 @@
-//ORGINIAL IDEA//The logic of the game is to have an array of choices and have the image match that word then the 4 choices //are displayed in the bottom that come from the another array of choices //if the button that was selected matches the image(word array) then the answer is correct//other choice will have displayed wrong and go to the next one
-//Function hides all the information from a page $(document).ready(function() {    $('#statext').hide();    $('#image').hide();    $('#distext').hide();    $('#button').hide();});
-//When the start button is clicked it starts the game $("#startClock").click(function(){    startTimer();    $('#startClock').hide();    $('#statext').show();    $('#image').show();    $('#distext').show();    $('#button').show(); });
-// This array holds the words we are going to choose from.//need to add 1 morevar words = ['eleven', 'demogorgon', 'barbara', 'will', 'lucas'];var choices = [['eleven', 'dustin', 'joyce', 'nancy'] , ['lucas', 'dustin', 'joyce', 'nancy'] , ['eleven', 'dustin', 'joyce', 'nancy'] , ['eleven', 'dustin', 'joyce', 'nancy'] , ['eleven', 'dustin', 'joyce', 'nancy'] , ['eleven', 'dustin', 'joyce', 'nancy']]
+//ORGINIAL IDEA
+//The logic of the game is to have an array of choices and have the image match that word then the 4 choices 
+//are displayed in the bottom that come from the another array of choices 
+//if the button that was selected matches the image(word array) then the answer is correct
+//other choice will have displayed wrong and go to the next one
+//Function hides all the information from a page 
+$(document).ready(function() {    $('#statext').hide();    $('#image').hide();    $('#distext').hide();    $('#button').hide();});
+//When the start button is clicked it starts the game 
+$("#startClock").click(function(){    startTimer();    $('#startClock').hide();    $('#statext').show();    $('#image').show();    $('#distext').show();    $('#button').show(); });
+// This array holds the words we are going to choose from.//need to add 1 more
+var words = ['eleven', 'demogorgon', 'barbara', 'will', 'lucas'];var choices = [['eleven', 'dustin', 'joyce', 'nancy'] , ['lucas', 'dustin', 'joyce', 'nancy'] , ['eleven', 'dustin', 'joyce', 'nancy'] , ['eleven', 'dustin', 'joyce', 'nancy'] , ['eleven', 'dustin', 'joyce', 'nancy'] , ['eleven', 'dustin', 'joyce', 'nancy']]
 //The problem with having this is that some random choices are going to be duplicates and sometimes even the random choice in the button //won't be there to select - there will have to be a lot of if and else statements //var choices = ['eleven', 'dustin', 'mike', 'lucas', 'joyce', 'will', 'nancy', 'billy', 'max', 'demogorgon','barbara'];
-//chooses a random word var rand = parseInt(Math.random() * words.length);var word = words[rand];
+//chooses a random word 
+var rand = parseInt(Math.random() * words.length);var word = words[rand];
 //choose four random choices //var randchoice1 = parseInt(Math.random() * choices.length);//var randchoice2 = parseInt(Math.random() * choices.length);//var randchoice3 = parseInt(Math.random() * choices.length);//var randchoice4 = parseInt(Math.random() * choices.length);
 //assigns that random word to word  
 //assigns four random choices  //var choice1 = choices[randchoice1];//var choice2 = choices[randchoice2];//var choice3 = choices[randchoice3];//var choice4 = choices[randchoice4];
